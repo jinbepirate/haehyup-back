2b745cae0dd4ba0278b3ad3d883867b1acfe3041
Elandland <bodan99@gmail.com>
2024-05-29 11:46:42 +0900

    Feat. 테마 사진 추가, 사용자 프로필 사진 추가 Feat. 로그인한 유저 정보 조회 api추가 Refactor. 불필요한 코드 삭제

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,58 +1,72 @@
-var express = require('express');
-var router = express.Router();
-const User = require("../../models/User");
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User');
 const auth = require('../../util/auth');
 
+// 회원가입 API
+router.post('/signup', async (req, res, next) => {
+  try {
+    const { uid, password } = req.body;
+    console.log(req.body);
+    const user = await User.signUp(uid, password);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    next(err);
+  }
+});
+
+// 로그인 API
+router.post('/login', async (req, res, next) => {
+  try {
+    const { uid, password } = req.body;
+    const user = await User.login(uid, password);
+    const tokenMaxAge = 60 * 60 * 24 * 3;
+    const token = auth.createToken(user, tokenMaxAge);
+
+    user.token = token;
+
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      maxAge: tokenMaxAge * 1000,
+    });
+    console.log(user);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    next(err);
+  }
+});
+
+// 로그아웃 API
+router.all('/logout', async (req, res, next) => {
+  try {
+    res.cookie("authToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    });
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    next(err);
+  }
+});
+
+// 로그인한 사용자의 정보 반환 API
+router.get('/me', auth.authenticate, auth.loginRequired, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select('-password -__v');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Error fetching user information', error: err.message });
+  }
+});
+
 module.exports = router;
-
-router.post("/signup", async (req, res, next) => {
-    try {
-      const { uid, password } = req.body;
-      console.log(req.body);
-      const user = await User.signUp(uid, password);
-      res.status(201).json(user);
-    } catch (err) {
-      console.error(err);
-      res.status(400);
-      next(err);
-    }
-  });
-
-  
-router.post("/login", async (req, res, next) => {
-    try {
-      const { uid, password } = req.body;
-      const user = await User.login(uid, password);
-      const tokenMaxAge = 60 * 60 * 24 * 3;
-      const token = auth.createToken(user, tokenMaxAge);
-      
-      user.token = token;
-      
-      res.cookie("authToken", token, {
-        httpOnly: true,
-        maxAge: tokenMaxAge * 1000,
-      });
-      console.log(user);
-      res.status(201).json(user);
-    } catch (err) {
-      console.error(err);
-      res.status(400);
-      next(err);
-    }
-  });
-  
-  router.all("/logout", async (req, res, next) => {
-    try {
-      //쿠키 삭제
-      res.cookie("authToken", "", {
-        httpOnly: true,
-        expires: new Date(Date.now()),
-      });
-      res.status(204).send();
-      
-    } catch (err) {
-      console.error(err);
-      res.status(400);
-      next(err);
-    }
-  });
